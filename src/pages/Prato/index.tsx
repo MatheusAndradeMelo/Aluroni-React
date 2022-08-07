1e862b5d929fd5aabcf67d28cee88c9c378a8656
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-08-07 15:45:05 -0300

    iniciated repository, the project is almost ready

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,36 @@
+import styles from './Prato.module.scss'; 
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+
+export default function Prato() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const prato = cardapio.find(item => item.id === Number(id));
+  if(!prato) {
+    return <NotFound />;
+  }
+  return (
+    <>
+      <button className={styles.voltar} onClick={() => navigate(-1)}>
+        {'< Voltar'}
+      </button>
+      <section className={styles.container}>
+        <h1 className={styles.titulo}>
+          {prato.title}
+        </h1>
+        <div className={styles.imagem}>
+          <img src={prato.photo} alt={prato.title} />
+        </div>
+        <div className={styles.conteudo}>
+          <p className={styles.conteudo__descricao}>
+            {prato.description}
+          </p>
+          <TagsPrato {...prato}/>
+        </div>
+      </section>
+    </>
+  );
+}
